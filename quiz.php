a5de45052c5bb8b3b032d9a78be19cb01c5eb12e
Xcaliburr-AT <2020120085@psu.palawan.edu.ph>
2024-12-14 20:48:54 +0800

    quiz.php html template

---
+++ quiz.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Math Quiz</title>
+</head>
+<body>
+    <div class="all">
+        <h1>Math Quiz</h1>
+    </div>
+</body>
+</html>
